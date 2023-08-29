d24247779252b7455e4635bab7fc072db7785c19
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-08-29 14:37:23 +0000

    default feature added

--- scripts/bgChanger.js
+++ scripts/bgChanger.js
@@ -3,6 +3,11 @@ let body = document.getElementsByTagName('body')[0];
 let buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => button.addEventListener('click', e => {
+    if (e.target.className === 'white') {
+        body.style.color = 'black';
+    } else {
+        body.style.color = 'whitesmoke';
+    }
+
     body.style.background = e.target.className;
-    body.style.color = 'whitesmoke';
 }));
