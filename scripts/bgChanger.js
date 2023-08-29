78d86cedc7e55d19198365accbd60c768716a43c
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-08-29 14:35:27 +0000

    optimized js for bgChanger

--- scripts/bgChanger.js
+++ scripts/bgChanger.js
@@ -3,6 +3,6 @@ let body = document.getElementsByTagName('body')[0];
 let buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => button.addEventListener('click', e => {
-    body.style.background = button.className;
+    body.style.background = e.target.className;
     body.style.color = 'whitesmoke';
 }));
