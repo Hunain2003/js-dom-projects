c436ec5dd673e919cdcb5f4d5e4fb6d3f0dbd2b7
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-08-31 19:03:32 +0000

    number guesser script completed

--- scripts/number-guessing-game.js
+++ scripts/number-guessing-game.js
@@ -4,15 +4,16 @@ let CHECK_BTN = document.querySelector('#check-guess-btn');
 let MIN_BOX = document.querySelector('#min');
 let MAX_BOX = document.querySelector('#max');
 let GUESS_AREA = document.querySelector('.guess-area');
+let TRY_ANOTHER = document.querySelector('#tryAnother');
 
 function generateNum() {
     let error = document.querySelector('error_msg');
     error = '';
 
-    let min = Number(MIN_BOX);
-    let max = Number(MAX_BOX);
+    let min = Number(MIN_BOX.value);
+    let max = Number(MAX_BOX.value);
     
-    if (!min && !max) {
+    if (!min || !max) {
         error.innerText = "Please enter only numbers in minimum and maximum input boxes";
     }
 
@@ -28,7 +29,6 @@ function checkGuess() {
     let guess = Number(document.querySelector('#guess').value);
     let message = document.querySelector('#message');
     let chances = document.querySelector('#left-count');
-    let tryAnother = document.querySelector('#tryAnother');
 
     if (!guess) {
         message.innerText = "Please only enter a number in guess";
@@ -36,24 +36,34 @@ function checkGuess() {
     } else if (guess == ORIGINAL_NUMBER) {
         message.innerText = "Congratulations you've guessed it";
         message.style.color = 'green';
+        TRY_ANOTHER.style.display = 'block';
+        CHECK_BTN.disabled = true;
     } else {
-        if (ORIGINAL_NUMBER - guess < 10) {
-            message.innerText = "Very nice guess. You are too close. Try again!";
-            message.style.color = 'blue';
-            chances.innerText = Number(chances.innerText--);
-            tryAnother.style.display = 'block';
-            CHECK_BTN.disabled = true;
+        chancesLeft = Number(chances.innerText);
+        if (chancesLeft == 1) {
+            message.innerText = "You've runout of chances";
+            message.style.color = 'red';
+            TRY_ANOTHER.style.display = 'block';
         } else {
-            message.innerText = "You are far from the number. Try again!";
-            message.style.color = 'blue';
+            if (Math.abs(ORIGINAL_NUMBER - guess) < 10) {
+                message.innerText = "Very nice guess. You are too close. Try again!";
+                message.style.color = 'blue';
+            } else {
+                message.innerText = "You are far from the number. Try again!";
+                message.style.color = 'red';
+            }
+            chances.innerText = --chancesLeft;
         }
     }
 }
 
 function reActivate() {
-    CHECK_BTN.disabled = false;
-    GENERATE_BTN.disabled = false;
+    CHECK_BTN.removeAttribute("disabled");
+    GENERATE_BTN.removeAttribute("disabled");
     MIN_BOX.value = '';
     MAX_BOX.value = '';
+    document.querySelector('#guess').value = '';
     GUESS_AREA.style.display = 'none';
+    TRY_ANOTHER.style.display = 'none';
+    document.querySelector('#left-count').innerText = 5;
 }
