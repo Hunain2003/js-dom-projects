565ffb7d93fd33798f5145ad0cfe773b5d652ddb
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-08-31 17:30:26 +0000

    bugs fixed in number guessing

--- scripts/number-guessing-game.js
+++ scripts/number-guessing-game.js
@@ -6,9 +6,16 @@ let MAX_BOX = document.querySelector('#max');
 let GUESS_AREA = document.querySelector('.guess-area');
 
 function generateNum() {
+    let error = document.querySelector('error_msg');
+    error = '';
+
     let min = Number(MIN_BOX);
     let max = Number(MAX_BOX);
     
+    if (!min && !max) {
+        error.innerText = "Please enter only numbers in minimum and maximum input boxes";
+    }
+
     let difference = max - min;
     
     ORIGINAL_NUMBER = Math.round(Math.random() * difference) + min;
@@ -21,7 +28,8 @@ function checkGuess() {
     let guess = Number(document.querySelector('#guess').value);
     let message = document.querySelector('#message');
     let chances = document.querySelector('#left-count');
-    
+    let tryAnother = document.querySelector('#tryAnother');
+
     if (!guess) {
         message.innerText = "Please only enter a number in guess";
         message.style.color = 'red';
@@ -29,12 +37,15 @@ function checkGuess() {
         message.innerText = "Congratulations you've guessed it";
         message.style.color = 'green';
     } else {
-        if (ORIGINAL_NUMBER - guess) {
+        if (ORIGINAL_NUMBER - guess < 10) {
             message.innerText = "Very nice guess. You are too close. Try again!";
             message.style.color = 'blue';
             chances.innerText = Number(chances.innerText--);
             tryAnother.style.display = 'block';
             CHECK_BTN.disabled = true;
+        } else {
+            message.innerText = "You are far from the number. Try again!";
+            message.style.color = 'blue';
         }
     }
 }
