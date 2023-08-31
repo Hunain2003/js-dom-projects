7731b357644553ff07d55a8d68d5baea62e5c8f4
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-08-31 17:05:09 +0000

    checkGuess implemented

--- scripts/number-guessing-game.js
+++ scripts/number-guessing-game.js
@@ -1,8 +1,28 @@
-let min = 4;
-let max = 5;
+let ORIGINAL_NUMBER = 0;
 
-let difference = max - min;
+function generateNum() {
+    let min = Number(document.querySelector('#min').value);
+    let max = Number(document.querySelector('#max').value);
+    
+    let difference = max - min;
+    
+    ORIGINAL_NUMBER = Math.round(Math.random() * difference) + min;
+}
 
-let randomNum = Math.round(Math.random() * difference) + min;
+function checkGuess() {
+    let guess = Number(document.querySelector('#guess').value);
+    let message = document.querySelector('#message');
 
-console.log(randomNum)
+    if (!guess) {
+        message.innerText = "Please only enter a number in guess";
+        message.style.color = 'red';
+    } else if (guess == ORIGINAL_NUMBER) {
+        message.innerText = "Congratulations you've guessed it";
+        message.style.color = 'green';
+    } else {
+        if (ORIGINAL_NUMBER - guess) {
+            message.innerText = "Very nice guess. You are too close. Try again!";
+            message.style.color = 'blue';
+        }
+    }
+}
